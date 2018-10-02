f79f5874f7e2446ff3c822194056f2a0c54c9770
Jonathan Lucki <43425183+jonathanlucki@users.noreply.github.com>
2018-10-01 21:04:30 -0400

    created resolution-overview.php

--- pages/delegation/resolution-overview.php
+++ pages/delegation/resolution-overview.php
@@ -7,3 +7,27 @@
  * Created: 10/1/18
  * Last Modified: 10/1/18
  */
+
+//Includes initialization file (init.php)
+include_once('../../resources/init.php');
+
+include('../layouts/header.php');
+include('../layouts/delegation-content-pane-start.php');
+
+//Sets resolution row
+$resolutionRow = getResolutionRow($_GET['num']);
+
+?>
+
+    <p class="lead">Resolution <?php echo $resolutionRow['num']?></p>
+    <p class="lead"><?php echo $resolutionRow['title']?></p>
+    <br>
+<?php include('../elements/delegates-table.php')?>
+    <br>
+    <p class="lead">Amendments:</p>
+<?php include('../elements/resolution-box.php')?>
+
+
+<?php
+include('../layouts/footer.php');
+include('../layouts/delegation-content-pane-end.php');
