a3cb8c3526e7828e0a0dc6fbf9dc5b6fc08552e5
Jonathan Lucki <43425183+jonathanlucki@users.noreply.github.com>
2018-10-01 20:29:00 -0400

    created resolutions.php file

---
+++ pages/delegation/resolutions.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Jonathan Lucki
+ * MMUNx
+ * File: resolutions.php
+ * Purpose:
+ * Created: 10/1/18
+ * Last Modified: 10/1/18
+ */
+
+//Includes initialization file (init.php)
+include_once('../../resources/init.php');
+
+include('../layouts/header.php');
+include('../layouts/delegation-content-pane-start.php');
+
+?>
+
+
+<?php include('../elements/resolutions-table.php')?>
+
+
+<?php
+include('../layouts/footer.php');
+include('../layouts/delegation-content-pane-end.php');
