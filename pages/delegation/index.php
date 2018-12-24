e0ba9c35ad7c699511df1986df453da41a039279
Jonathan Lucki <43425183+jonathanlucki@users.noreply.github.com>
2018-12-23 20:46:58 -0500

    Combined admin and delegation content panes into just one content pane

--- pages/delegation/index.php
+++ pages/delegation/index.php
@@ -12,7 +12,7 @@
 include_once('../../resources/init.php');
 
 include('../layouts/header.php');
-include('../layouts/delegation-content-pane-start.php');
+include('../layouts/content-pane-start.php');
 
 //Sets country row
 $countryRow = getCountryRow($_SESSION['countryID']);
@@ -26,5 +26,5 @@ $countryRow = getCountryRow($_SESSION['countryID']);
 <p class="lead text-center">To get started, navigate to <em>Country Overview</em> to submit an amendment.</p>
 
 <?php
-include('../layouts/delegation-content-pane-end.php');
+include('../layouts/content-pane-end.php');
 include('../layouts/footer.php');
--- pages/layouts/content-pane-end.php
+++ pages/layouts/content-pane-end.php
@@ -2,7 +2,7 @@
 /**
  * Jonathan Lucki
  * MMUNx
- * File: admin-content-pane-end.php
+ * File: content-pane-end.php
  * Purpose:
  * Created: 9/15/18
  * Last Modified: 9/15/18
