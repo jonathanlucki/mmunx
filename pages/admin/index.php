c48ec6a2b9c3ce7c7037ab0de392326c3a71f8a0
Jonathan Lucki <43425183+jonathanlucki@users.noreply.github.com>
2019-01-25 01:23:08 -0500

    updating heroku repository

---
+++ pages/admin/index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Jonathan Lucki
+ * MMUNx
+ * File: index.php
+ * Purpose:
+ * Created: 1/25/19
+ * Last Modified: 1/25/19
+ */
+
+//Includes initialization file (init.php)
+include_once('../../resources/init.php');
+
+include('../layouts/header.php');
+include('../layouts/admin-navbar.php');
+
+?>
+
+    <h1 class="display-4 text-center">Welcome, admin.</h1>
+
+<?php
+include('../layouts/footer.php');
