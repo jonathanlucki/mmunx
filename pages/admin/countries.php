ee41f4e48cc70576ef649162663dee021d4683ef
Jonathan Lucki <43425183+jonathanlucki@users.noreply.github.com>
2019-01-26 00:42:28 -0500

    created countries.php

---
+++ pages/admin/countries.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Jonathan Lucki
+ * MMUNx
+ * File: countries.php
+ * Purpose:
+ * Created: 1/26/19
+ * Last Modified: 1/26/19
+ */
+
+//Includes initialization file (init.php)
+include_once('../../resources/init.php');
+
+include('../layouts/header.php');
+include('../layouts/admin-navbar.php');
+
+
+include('../elements/country-table.php');
+
+
+include('../layouts/footer.php');
