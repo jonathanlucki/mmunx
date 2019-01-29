0a4f2282345f7d741fc9b162246d7a235685db6b
Jonathan Lucki <43425183+jonathanlucki@users.noreply.github.com>
2019-01-29 03:00:20 -0500

    created delete-resolution-button.php element

--- resources/paths.php
+++ resources/paths.php
@@ -22,6 +22,7 @@ define("PATHS",array(
     'create-resolution-button.php' => '/pages/elements/create-resolution-button.php',
     'delegates-table.php' => '/pages/elements/delegates-table.php',
     'delete-country-button.php' => '/pages/elements/delete-country-button.php',
+    'delete-resolution-button.php' => '/pages/elements/delete-resolution-button.php',
     'edit-country-button.php' => '/pages/elements/edit-country-button.php',
     'edit-resolution-button.php' => '/pages/elements/edit-resolution-button.php',
     'resolution-box-class.php' => '/pages/elements/resolution-box-class.php',
