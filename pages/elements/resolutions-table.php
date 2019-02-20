7875dba66522b30aa66e450603e2f58614948b60
Jonathan Lucki <43425183+jonathanlucki@users.noreply.github.com>
2019-02-20 07:59:31 -0500

    added status column to resolutions-table.php

--- pages/elements/resolutions-table.php
+++ pages/elements/resolutions-table.php
@@ -13,15 +13,20 @@
     <thead>
     <tr>
         <th scope="col">Resolution</th>
+        <th scope="col">Status</th>
         <th scope="col">Amendment Count</th>
     </tr>
     </thead>
     <tbody>
     <?php
     for ($rowNum=1; $rowNum <= getResolutionCount(); $rowNum++) {
+        $resolutionRow = getResolutionRow($rowNum);
         echo '<tr>';
         echo '<td>';
-        echo '<a href="'.getLocalFilePath('resolution-overview.php').'?num=' . $rowNum . '"> Resolution ' . $rowNum . ': ' . getResolutionRow($rowNum)['title'] . '</a>';
+        echo '<a href="'.getLocalFilePath('resolution-overview.php').'?num=' . $rowNum . '"> Resolution ' . $rowNum . ': ' . $resolutionRow['title'] . '</a>';
+        echo '</td>';
+        echo '<td>';
+        echo convertResolutionStatus($resolutionRow['status']);
         echo '</td>';
         echo '<td>';
         echo getAmendmentCountByResolutionNum($rowNum);
